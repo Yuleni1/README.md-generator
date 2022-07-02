10f3569f09a74531d3ef5512845db154b09fdc26
Yuleni1 <myuleni4@gmail.com>
2022-07-02 13:30:15 -0400

    questions populating on terminal

---
+++ index.js
@@ -0,0 +1,74 @@
+// TODO: Include packages needed for this application
+const fs = require('fs');
+//error path 
+const path = require('path');
+const inquirer = require('inquirer');
+// TODO: Create an array of questions for user input
+//question:they put it in a function and this is just an array of objects
+const markdown = require("./utils/generateMarkdown")
+//they use return inquirer.prompt() and then they put the questions inside? 
+const questions = [
+    {
+        type:'input',
+        name:'name',
+        message:'What is your project title?'  
+    },
+    {
+        type:'input',
+        name:'description',
+        message:'What is your project description?'  
+    },
+    {
+        type:'input',
+        name:'Installation',
+        message:'What are the steps required to install your project?'  
+    },
+    {
+        type:'input',
+        name:'usage',
+        message:'Provide instructions and examples for use.'  
+    },
+    {
+        type:'input',
+        name:'contribution',
+        message:'Provide contribution guidelines.'  
+    },
+    {
+        type:'input',
+        name:'tests',
+        message:'Provide examples on how to run tests.'  
+    }
+
+];
+
+// TODO: Create a function to write README file
+//fs.write 
+function writeToFile(fileName, data) {
+    return new Promise(()=>{
+    fs.writeFile('README.md',fileName, data, err =>{
+
+        if (err) {
+            console.error(err)
+            return
+        }
+    
+        // Log this message if the file was written to successfully
+        console.log('wrote to file successfully')
+  })
+})
+}
+
+//is this where they want me to write the html code? 
+
+
+// TODO: Create a function to initialize app
+//inialize what?
+//inquirer.prompt ([ iterate through the questions])
+function init() {
+    inquirer.prompt(questions)
+}
+
+// Function call to initialize app
+init();
+
+
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,9 +1,18 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+
+function renderLicenseBadge(license) {
+//if they had answered none icon
+  if(license !== "none"){
+    return`![GitHub license](https://img.shields.io/badge/license-${license}-blue.svg)` ;
+  }
+  return "";
+
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
+//[]
 function renderLicenseLink(license) {}
 
 // TODO: Create a function that returns the license section of README
@@ -11,6 +20,7 @@ function renderLicenseLink(license) {}
 function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
+//this will have the #, ##, * for the read me file 
 function generateMarkdown(data) {
   return `# ${data.title}
 
